4f07fcd375b0939b70022273b213b6728a7b56ae
ValeriaLes <valeria.l.1994@outlook.com>
2023-08-29 15:37:44 +0200

    firstTask

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+
+const galleryItemsMarkup = galleryItems.map(({ preview, description }) => {
+    return `<li class="gallery__item">
+     <a class="gallery__link" href="">
+       <img
+         class="gallery__image"
+         src="${preview}"
+         data-source=""
+         alt="${description}"
+       />
+     </a>
+   </li>`
+   
+})
+.join("")
+
+
+galleryEl.innerHTML = galleryItemsMarkup
+// const linkEl = document.querySelector('.gallery-link')
+
+// galleryEl.addEventListener('click', onPictureZoomClick )
+
+//  function onPictureZoomClick (event) {
+
+//     if (!event.target.classList.contains('gallery__link')){
+//         return
+//     }
+// linkEl.href = "galleryItems.original"
+
+// console.log(event.target)
+
+// }
+
 
