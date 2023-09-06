c20e5dea1bc3ae673ae2bece3b5294d6eda9b8d9
ValeriaLes <valeria.l.1994@outlook.com>
2023-09-07 00:20:27 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,41 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery')
+
+
+const galleryItemsMarkup = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+ </li>`
+
+   
+})
+.join("")
+
+
+
+galleryListEl.innerHTML = galleryItemsMarkup
+
+const simpleLightBox = new SimpleLightbox('.gallery__link');
+
+console.log(simpleLightBox.defaultOptions.captionsData)
+
+
+
+
+
+
+// console.log(simpleLightBox.defaultOptions)
+// const altAttr = image.getAttribute()
+// console.log(simpleLightBox.defaultOptions.captionsData = altAttr)
+
+ 
+
+
+
+
+  
